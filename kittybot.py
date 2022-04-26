8c6b264fdb95542cd287d4db5adfa2a96433601b
Макс Астафьев <astafiev.maksim@yandex.ru>
2022-04-26 16:42:26 +0300

    learn name

--- kittybot.py
+++ kittybot.py
@@ -1,42 +1,67 @@
-import requests
 import os
 
-from dotenv import load_dotenv
+import requests
+
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+from telegram.ext import CommandHandler, Updater, Filters, MessageHandler
+
+from dotenv import load_dotenv 
 
 load_dotenv()
 
 secret_token = os.getenv('TOKEN')
-updater = Updater(token=secret_token)
+
 URL = 'https://api.thecatapi.com/v1/images/search'
 
+
 def get_new_image():
-    response = requests.get(URL).json()
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        print(error)
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    
+    response = response.json()
     random_cat = response[0].get('url')
     return random_cat
+
 
 def new_cat(update, context):
     chat = update.effective_chat
     context.bot.send_photo(chat.id, get_new_image())
+
 
 def say_hi(update, context):
     chat = update.effective_chat
     context.bot.send_message(chat_id=chat.id,
                              text='Привет, я KittyBot!')
 
+
 def wake_up(update, context):
     chat = update.effective_chat
     name = update.message.chat.first_name
     button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
-    context.bot.send_message(chat_id=chat.id, 
-                             text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
-                             reply_markup=button
-                             )
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}. Посмотри какого котика я тебе нашел'.format(name),
+        reply_markup=button
+    )
+
     context.bot.send_photo(chat.id, get_new_image())
 
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
-updater.start_polling()
-updater.idle() 
+
+def main():
+    updater = Updater(token=secret_token)
+
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
